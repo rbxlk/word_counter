689b72e354d207c128e1b3e1526d5e4347682d2a
rbxlk <rbxlk@protonmail.com>
2021-05-13 14:03:46 +0100

    Create hash to count words and template block for iteration

--- word_counter.rb
+++ word_counter.rb
@@ -1,2 +1,18 @@
 TEXT_FILE = "romeo-juliet.txt"
-romeo_and_juliet = File.read(TEXT_FILE).downcase.gsub(/[^a-z]/, " ").split
+
+# Load the words from a file
+def words_from_file(text_file)
+    File.read(TEXT_FILE).downcase.gsub(/[^a-z]/, " ").split
+end
+
+# Load the list of words in the text
+words = words_from_file(TEXT_FILE)
+
+# Create dictionary of word counts
+word_count = {}
+words.each do |word|
+    # If this word is new, add it to word_count
+    word_count[word] = 1
+    # ... else increment its count
+    word_count[word] += 1
+end
