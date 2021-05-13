a42f4e31b0d45ba28186f8c1a936daf83c64379a
rbxlk <rbxlk@protonmail.com>
2021-05-13 15:21:20 +0100

    Add rescue when text file not provided

--- word_counter.rb
+++ word_counter.rb
@@ -2,7 +2,10 @@ TEXT_FILE = "romeo-juliet.txt"
 
 # Load the words from a file
 def words_from_file(text_file)
-    File.read(TEXT_FILE).downcase.gsub(/[^a-z]/, " ").split
+    File.read(text_file).downcase.gsub(/[^a-z]/, " ").split
+rescue
+    puts "Please provide the following file: #{text_file}"
+    exit
 end
 
 # Load the list of words in the text
@@ -12,7 +15,7 @@ words = words_from_file(TEXT_FILE)
 word_count = {}
 words.each do |word|
     # If this word is new, add it to word_count
-    word_count[word] = 1
+    word_count[word] = 0 unless word_count[word]
     # ... else increment its count
     word_count[word] += 1
 end
